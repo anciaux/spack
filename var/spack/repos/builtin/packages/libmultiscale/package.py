78cd45d24f26524208fbb9f6fcc5605ea51a8501
Guillaume Anciaux <guillaume.anciaux@epfl.ch>
2021-03-25 15:09:03 +0100

    starting the libmultiscale spack package

---
+++ var/spack/repos/builtin/packages/libmultiscale/package.py
@@ -0,0 +1,51 @@
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+# ----------------------------------------------------------------------------
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install libmultiscale-git
+#
+# You can edit this file again by typing:
+#
+#     spack edit libmultiscale-git
+#
+# See the Spack documentation for more information on packaging.
+# ----------------------------------------------------------------------------
+
+from spack import *
+
+
+class Libmultiscale(CMakePackage):
+    """LibMultiScale is a C++/python parallel framework for the multiscale
+       coupling methods dedicated to material simulations """
+
+    homepage = "hhttps://libmultiscale.gitlab.io/libmultiscal"
+    git = "https://gitlab.com/libmultiscale/libmultiscale.git"
+
+    maintainers = ['anciaux']
+
+    version('lammps_2020', branch='lammps_2020',
+            submodules=True, submodules_delete=['third-party/akantu'])
+
+    depends_on('akantu@master', )
+
+    def cmake_args(self):
+        spec = self.spec
+        args = [
+            '-DCMAKE_BUILD_TYPE:STRING=$BUILD_TYPE',
+            '-DLIBMULTISCALE_AKANTU_PLUGIN:BOOL=ON',
+            '-DLIBMULTISCALE_MD1D:BOOL=ON',
+            '-DLIBMULTISCALE_MECA1D:BOOL=ON',
+            '-DLIBMULTISCALE_LAMMPS_PLUGIN:BOOL=ON',
+            '-DLIBMULTISCALE_DOCUMENTATION:BOOL=ON ',
+            '-DLIBMULTISCALE_TESTS:BOOL=ON'
+        ]
+        return args
